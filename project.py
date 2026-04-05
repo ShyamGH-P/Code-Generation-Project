4f14355adbdaa94848dc090632b29f8d7a065eec
ShyamGH-P <shyam.essampalli@outlook.com>
2026-04-05 21:02:21 +0100

    Iteration 1: fixed grade case mismatch & validated class distribution

--- project.py
+++ project.py
@@ -3,18 +3,28 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score, classification_report
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 # Load dataset
 df = pd.read_csv("Student_Performance_new.csv")
 
 # Convert final_grade into pass/fail
 def grade_to_pass_fail(grade):
-    if grade in ['A', 'B', 'C']:
+    if grade.lower() in ['a', 'b', 'c']:
         return 'Pass'
     else:
         return 'Fail'
 
 df['pass_fail'] = df['final_grade'].apply(grade_to_pass_fail)
+
+#verifying class distribution
+print("The value counts in the pass/fail column are:/n",
+       df['pass_fail'].value_counts())
+
+sns.barplot(data=df['pass_fail'].value_counts())
+plt.title("Class Distribution of pass and fail")
+plt.show()
 
 # Drop columns not needed
 df = df.drop(['student_id', 'final_grade'], axis=1)
